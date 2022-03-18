39444baaa6f4474e8f3b5f0f8eeeccb48c21b8bf
Dumre2022 <gokarna.dumre@estudiantat.upc.edu>
2022-03-18 12:16:41 +0100

    v1 practica 2

---
+++ utils/MyServerSocket.py
@@ -0,0 +1,6 @@
+#Mateixa funcionalitat que ServerSocket però  s'encapsulen excepcions i els corresponents streams de text BufferedReader i PrintWriter. 
+#Aquestes classes hauran de disposar de mètodes de lectura/escriptura dels tipus bàsics.
+
+
+
+
